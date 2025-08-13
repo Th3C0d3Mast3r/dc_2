e195e6740a62bf92fa764336dd9ee2bd5a0ee42b
unknown <acharyadevesh1105@gmail.com>
2025-08-13 14:39:03 +0530

    intermediate changes

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,4 +1,4 @@
-import mongoose from mongoose
+import mongoose from 'mongoose'
 
 const connectDB= async ()=>{
     const DB_URI= "mongodb+srv://menildhameliya1234:C2pA4FhV5ZLCtopZ@cluster0.mg2kqsb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import connectDB from './db/db.js';
 
 const app= express();
+const conect = connectDB();
+
 const PORT = 3000;
 const GREEN='\x1b[32m';
 const RESET='\x1b[0m';
@@ -14,6 +17,9 @@ app.get('/kshitij', (req, res)=>{
     res.send("This is KSHITIJ'S ROUTE");
 });
 
+app.get('/getDetails', (req, res) => {
+  const id = req.query.id;
+});
 
 app.listen(PORT, () => {
   console.log(`${GREEN}Server is running${RESET} on http://localhost:${PORT}`);

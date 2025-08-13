67c8040b5587bb7247f4f37a36e65729a46d50ff
Menil-dev <menildhameliya1234@gmail.com>
2025-08-13 14:24:12 +0530

    DB Connection

---
+++ backend/db/db.js
@@ -0,0 +1,18 @@
+import mongoose from mongoose
+
+const connectDB= async ()=>{
+    const DB_URI= "mongodb+srv://menildhameliya1234:C2pA4FhV5ZLCtopZ@cluster0.mg2kqsb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    try{
+        const connection= await mongoose.connection("DB_URI",
+            {
+                newUrlParser: true
+            }
+        )
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+}
+
+export default connectDB;
